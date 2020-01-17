9992fda3d5841cd1385c625168c075351486ddb4
Félix Vallé <fvalle@jahia.com>
2020-01-17 12:08:06 +0100

    Change to trigger the workflow

--- src/colors/color.stories.jsx
+++ src/colors/color.stories.jsx
@@ -43,7 +43,7 @@ function displayColors(palette) {
     return colors;
 }
 
-storiesOf('Tokens|Colors', module)
+storiesOf('Tokens|Colors TEST', module)
     .add('Accent', () => (
         <section className={classnames(storyStyles.storyWrapper)}>
             {displayColors(paletteAccent)}
