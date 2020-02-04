30a7c8c242e6ad0e8ed72071f132fb86022fbedf
Félix Vallé <fvalle@jahia.com>
2020-02-04 16:01:44 +0100

    test update gh-pages

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -8,7 +8,7 @@ import Power from '~/icons/asset/Power.svg';
 import markdownNotes from './Button.md';
 import {Button} from './index';
 
-storiesOf('Components|Button XXX', module)
+storiesOf('Components|Button YY', module)
     .addParameters({
         component: Button,
         componentSubtitle: 'Button',
